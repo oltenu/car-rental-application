97511e636a8cb8ef846614457d2e53b9f4d2ff2d
Darius <oldarius2002@gmail.com>
2023-04-20 15:47:07 +0300

    Implemented logic

--- CarRental/src/main/java/logic/CarFactory.java
+++ CarRental/src/main/java/logic/CarFactory.java
@@ -1,4 +1,30 @@
 package logic;
 
+import model.car.*;
+
 public class CarFactory {
+    public static Car getCar(String brand){
+        switch (brand) {
+            case "Dacia" -> {
+                return new Dacia();
+            }
+            case "Renault" -> {
+                return new Renault();
+            }
+            case "Audi" -> {
+                return new Audi();
+            }
+            case "Ford" -> {
+                return new Ford();
+            }
+            case "Mercedes" -> {
+                return new Mercedes();
+            }
+            case "Volkswagen" -> {
+                return new Volkswagen();
+            }
+        }
+        System.out.println("Invalid brand!");
+        return null;
+    }
 }
--- CarRental/src/main/java/logic/RentalCompany.java
+++ CarRental/src/main/java/logic/RentalCompany.java
@@ -1,4 +1,59 @@
 package logic;
 
+import model.car.Car;
+import model.person.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RentalCompany {
+    List<Car> cars;
+
+    public RentalCompany() {
+        cars = new ArrayList<>();
+    }
+
+    public void acquireCar(String brand) {
+        cars.add(CarFactory.getCar(brand));
+    }
+
+    public void getCatalogue() {
+        for (Car car : cars) {
+            System.out.println("Brand: " + car.getBrandName() + "; Group: " + car.getGroup() + "; Max Speed: "
+                    + car.getMaxSpeed() + "; Horse Power: " + car.getHorsePower() + "; Mileage: " + car.getNumberOfKilometers());
+        }
+    }
+
+    public void rentCar(String brand, Person person, int hours) {
+        for (Car car : cars) {
+            if (car.getBrandName().equals(brand)) {
+                if (car.isAvailable()) {
+                    if (car.getPricePerHour() * hours <= person.getMoney()) {
+                        if (person.getRentedCar() == null) {
+                            car.setAvailable(false);
+                            person.setRentedCar(car);
+                            person.setMoney(person.getMoney() - car.getPricePerHour() * hours);
+                            System.out.println(person.getName() + " has rented a " + car.getBrandName());
+                        }else{
+                            System.out.println(person.getName() + " has already rented a car!");
+                        }
+                    }else{
+                        System.out.println(person.getName() + " doesn't have enough money!");
+                    }
+                }else{
+                    System.out.println(car.getBrandName() + " is not available!");
+                }
+            }
+        }
+    }
+
+    public void returnCar(Person person) {
+        for (Car car : cars) {
+            if (car == person.getRentedCar()) {
+                car.setAvailable(true);
+                person.setRentedCar(null);
+                System.out.println(person.getName() + " has returned a " + car.getBrandName() + "!");
+            }
+        }
+    }
 }
